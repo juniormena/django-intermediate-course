d59a7aeae7d1718aa4e5f0073041b536902f944f
unknown <juniormelpelotero@hotmail.com>
2020-01-11 11:04:10 -0400

    ..

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from news.views import scrape
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notes/', include('notepad.url'))
+    path('notes/', include('notepad.url')),
+    path('scrape/', scrape)
 ]
+
+if settings.DEBUG:
+    urlpatterns == static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns == static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
